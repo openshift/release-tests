41ceeea19fbbb2e2db4a0d6cc4dae51e6198afa5
Rio Liu <rioliu@redhat.com>
2023-04-25 16:03:46 +0800

    remove exception raising logic in util functions

--- oar/core/util.py
+++ oar/core/util.py
@@ -13,10 +13,8 @@ def is_valid_z_release(version):
 
 
 def get_y_release(version):
-    if version is None:
-        raise ValueError("argument version is required")
     strs = version.split(".")
     if len(strs) > 2:
         return "%s.%s" % (strs[0], strs[1])
-    else:
-        raise ValueError(f"invalid release version {version}")
+
+    return None
