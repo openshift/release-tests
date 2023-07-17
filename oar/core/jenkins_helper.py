40671f33ebece281e2953f4c39dec62f7be96657
Rio Liu <rioliu@redhat.com>
2023-07-17 10:59:44 +0800

    fix regex expression issue

--- oar/core/jenkins_helper.py
+++ oar/core/jenkins_helper.py
@@ -6,71 +6,125 @@ import oar.core.util as util
 from oar.core.exceptions import JenkinsHelperException
 
 logger = logging.getLogger(__name__)
+
+
 class JenkinsHelper:
     def __init__(self, cs: ConfigStore):
         self._cs = cs
         self.version = util.get_y_release(self._cs.release)
-        self.vversion = "v"+self.version
+        self.vversion = "v" + self.version
         self.metadata_ad = self._cs.get_advisories()["metadata"]
-        #get string errata_numbers like: 115076 115075 115077 115074 115078
-        self.errata_numbers = ' '.join([str(i)for i in [val for val in self._cs.get_advisories().values()]])
-        self.pull_spec = "quay.io/openshift-release-dev/ocp-release:" +self._cs.release+ "-x86_64"
-        self.zstream_url = self._cs.get_jenkins_server()+ "/job/zstreams/"
+        # get string errata_numbers like: 115076 115075 115077 115074 115078
+        self.errata_numbers = " ".join(
+            [str(i) for i in [val for val in self._cs.get_advisories().values()]]
+        )
+        self.pull_spec = (
+            "quay.io/openshift-release-dev/ocp-release:" + self._cs.release + "-x86_64"
+        )
+        self.zstream_url = self._cs.get_jenkins_server() + "/job/zstreams/"
 
-    def call_stage_job(self): 
+    def call_stage_job(self):
         try:
-            server = jenkins.Jenkins(self.zstream_url, username=self._cs.get_jenkins_username(), password=self._cs.get_jenkins_token())
-            server.build_job("Stage-Pipeline",parameters={'VERSION': self.version,'METADATA_AD':self.metadata_ad,'PULL_SPEC':self.pull_spec})
-        except jenkins.JenkinsException as ej:   
+            server = jenkins.Jenkins(
+                self.zstream_url,
+                username=self._cs.get_jenkins_username(),
+                password=self._cs.get_jenkins_token(),
+            )
+            server.build_job(
+                "Stage-Pipeline",
+                parameters={
+                    "VERSION": self.version,
+                    "METADATA_AD": self.metadata_ad,
+                    "PULL_SPEC": self.pull_spec,
+                },
+            )
+        except jenkins.JenkinsException as ej:
             raise JenkinsHelperException("call stage pipeline job failed") from ej
-        last_build_number = server.get_job_info("Stage-Pipeline")['lastBuild']['number']
+        last_build_number = server.get_job_info("Stage-Pipeline")["lastBuild"]["number"]
         if not last_build_number:
-            raise JenkinsHelperException("Trigger stage pipeline job failed: cannot get latest build number")
-        return self.zstream_url+ "job/Stage-Pipeline/" +str(last_build_number)
-    
-    def call_image_consistency_job(self): 
+            raise JenkinsHelperException(
+                "Trigger stage pipeline job failed: cannot get latest build number"
+            )
+        return self.zstream_url + "job/Stage-Pipeline/" + str(last_build_number)
+
+    def call_image_consistency_job(self):
         try:
-            server = jenkins.Jenkins(self._cs.get_jenkins_server(), username=self._cs.get_jenkins_username(), password=self._cs.get_jenkins_token())
-            server.build_job("image-consistency-check",parameters={'VERSION': self.vversion,'ERRATA_NUMBERS':self.errata_numbers,'PAYLOAD_URL':self.pull_spec})
-        except jenkins.JenkinsException as ej:   
-            raise JenkinsHelperException("call image-consistency-check pipeline job failed") from ej
-        last_build_number = server.get_job_info("image-consistency-check")['lastBuild']['number']
+            server = jenkins.Jenkins(
+                self._cs.get_jenkins_server(),
+                username=self._cs.get_jenkins_username(),
+                password=self._cs.get_jenkins_token(),
+            )
+            server.build_job(
+                "image-consistency-check",
+                parameters={
+                    "VERSION": self.vversion,
+                    "ERRATA_NUMBERS": self.errata_numbers,
+                    "PAYLOAD_URL": self.pull_spec,
+                },
+            )
+        except jenkins.JenkinsException as ej:
+            raise JenkinsHelperException(
+                "call image-consistency-check pipeline job failed"
+            ) from ej
+        last_build_number = server.get_job_info("image-consistency-check")["lastBuild"][
+            "number"
+        ]
         if not last_build_number:
-            raise JenkinsHelperException("Trigger image check job failed: cannot get latest build number")
-        return self._cs.get_jenkins_server()+ "/job/image-consistency-check/" +str(last_build_number)  
-    
+            raise JenkinsHelperException(
+                "Trigger image check job failed: cannot get latest build number"
+            )
+        return (
+            self._cs.get_jenkins_server()
+            + "/job/image-consistency-check/"
+            + str(last_build_number)
+        )
+
     def get_job_status(self, url, job_name, build_number):
         """
         get job status via build_number
         """
         try:
-            server = jenkins.Jenkins(url, username=self._cs.get_jenkins_username(), password=self._cs.get_jenkins_token())
+            server = jenkins.Jenkins(
+                url,
+                username=self._cs.get_jenkins_username(),
+                password=self._cs.get_jenkins_token(),
+            )
             server.assert_job_exists
             try:
                 build_info = server.get_build_info(job_name, build_number)
             except jenkins.JenkinsException as je:
-                raise JenkinsHelperException(f"job[{job_name}] number [{build_number}] does not exist") from je
-            
+                raise JenkinsHelperException(
+                    f"job[{job_name}] number [{build_number}] does not exist"
+                ) from je
+
             if job_name == "signature_check":
-                zstream_version = build_info['actions'][0]['parameters'][0]['value'] 
+                zstream_version = build_info["actions"][0]["parameters"][0]["value"]
             else:
-                pattern = "4.\d{2}.\d{1}|\d{2}}"
-                if (build_info['actions'][0]['_class']=='hudson.model.ParametersAction'):
-                    payload = build_info['actions'][0]['parameters'][2]['value']
+                pattern = "4.\d+.\d+"
+                if (
+                    build_info["actions"][0]["_class"]
+                    == "hudson.model.ParametersAction"
+                ):
+                    payload = build_info["actions"][0]["parameters"][2]["value"]
                 else:
-                    payload = build_info['actions'][1]['parameters'][2]['value']
-                match = re.search(pattern, payload) 
+                    payload = build_info["actions"][1]["parameters"][2]["value"]
+                match = re.search(pattern, payload)
                 if match:
                     zstream_version = match.group()
-            if zstream_version.find(self._cs.release)!=-1:
-                #the job is in progress or already finished
-                if build_info['building']:
-                    logger.info(f"{job_name} job: {build_number} status is: In Progress")
+            if zstream_version.find(self._cs.release) != -1:
+                # the job is in progress or already finished
+                if build_info["building"]:
+                    logger.info(
+                        f"{job_name} job: {build_number} status is: In Progress"
+                    )
                 else:
-                    status = server.get_build_info(job_name, build_number)['result']
+                    status = server.get_build_info(job_name, build_number)["result"]
                     logger.info(f"{job_name} job: {build_number} status is: {status}")
             else:
-                logger.error(f"{url}job/{job_name}/{build_number} version is not [{self._cs.release}], please provide correct job id")
+                logger.error(
+                    f"{url}job/{job_name}/{build_number} version is not [{self._cs.release}], please provide correct job id"
+                )
         except:
-            raise JenkinsHelperException("get job{job_name}:{build_number} status failed")
-
+            raise JenkinsHelperException(
+                "get job{job_name}:{build_number} status failed"
+            )
