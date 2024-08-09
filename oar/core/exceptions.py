6ab434eceeb9c8759b9cba722e5fe02fd72cde71
Rio Liu <rioliu@redhat.com>
2024-08-09 18:47:33 +0800

    refine error handling for existing worksheet

--- oar/core/exceptions.py
+++ oar/core/exceptions.py
@@ -6,17 +6,25 @@ class WorksheetException(BaseException):
     """Exception class to raise error in WorksheetManager"""
 
 
+class WorksheetExistsException(WorksheetException):
+    """Exception class to raise workhseet already exists error"""
+
+
 class JiraException(BaseException):
     """Exception class to raise error in JiraManager"""
+
 
 class JiraUnauthorizedException(JiraException):
     """Exception class to raise error in JiraManager when got 403 unauthorized"""
 
+
 class AdvisoryException(BaseException):
     """Exception class to raise error in AdvisoryManager"""
 
+
 class NotificationException(BaseException):
     """Exception class to raise error in notificationManager"""
+
 
 class JenkinsHelperException(BaseException):
     """Exception class to raise error in JenkinsHelper"""
