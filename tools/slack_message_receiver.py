30071a9cd51bcdf101293ec7cb38182a1036bfa6
Xiaozheng Liu (Rio) <rioliu@redhat.com>
2024-11-04 18:48:58 +0800

    OCPERT-11 integrate LLM with slack app (#345)

--- tools/slack_message_receiver.py
+++ tools/slack_message_receiver.py
@@ -3,6 +3,7 @@ from slack_sdk.socket_mode.request import SocketModeRequest
 from slack_sdk.socket_mode.response import SocketModeResponse
 from slack_sdk.web import WebClient
 from slack_sdk.socket_mode import SocketModeClient
+from langchain_community.llms.vllm import VLLMOpenAI
 import os
 import logging
 import re
@@ -49,6 +50,31 @@ def is_oar_related_message(message):
     return message.startswith("oar") or message.startswith("oarctl") or re.search("^<\@.*>\ oar\ ", message) or re.search("^<\@.*>\ oarctl\ ", message)
 
 
+def send_prompt_to_ai_model(prompt):
+    """
+    Send prompt message to LLM model and return the answer
+    We only support OpenAI-compatible API
+    """
+    model_api_base = os.environ.get("MODEL_API_BASE")
+    model_api_key = os.environ.get("MODEL_API_KEY")
+    model_api_name = os.environ.get("MODEL_API_NAME")
+
+    if not model_api_base or not model_api_key or not model_api_name:
+        logger.warning(
+            "environment variables MODEL_API_BASE/MODEL_API_KEY/MODEL_API_NAME are not defined")
+        return None
+
+    if prompt:
+        llm = VLLMOpenAI(
+            openai_api_key=model_api_key,
+            openai_api_base=f"{model_api_base}/v1",
+            model_name=model_api_name,
+        )
+        return llm.invoke(prompt)
+
+    return None
+
+
 def process(client: SocketModeClient, req: SocketModeRequest):
     # Acknowledge the request anyway
     response = SocketModeResponse(envelope_id=req.envelope_id)
@@ -91,6 +117,16 @@ def process(client: SocketModeClient, req: SocketModeRequest):
                         channel=channel_id,
                         thread_ts=thread_ts,
                         text=f"```{result.stdout}```")
+            else:
+                # forward message as prompt to LLM, this feature is optional
+                # if required system environment variables are not defined
+                # API call with AI model is disabled, none will be returned
+                answer = send_prompt_to_ai_model(message)
+                if answer:
+                    client.web_client.chat_postMessage(
+                        channel=channel_id,
+                        thread_ts=thread_ts,
+                        text=answer)
 
 
 # Add a new listener to receive messages from Slack
