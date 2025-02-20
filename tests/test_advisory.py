99c032e28099dc2b1ba62c285a4e90469027896d
ming1013 <minl@redhat.com>
2025-02-20 14:54:21 +0800

    add assert for testing

--- tests/test_advisory.py
+++ tests/test_advisory.py
@@ -70,5 +70,5 @@ class TestAdvisoryManager(unittest.TestCase):
             self.assertFalse(ad.has_blocking_secruity_alert(), f"advisory {ad.errata_id} has blocking security alerts")
 
     def test_kernel_tag(self):
-       Advisory(errata_id=144853, impetus='image').check_kernel_tag()
-       Advisory(errata_id=144854, impetus='metadata').check_kernel_tag()
+       self.assertTrue(Advisory(errata_id=144853, impetus='image').check_kernel_tag())
+       self.assertFalse(Advisory(errata_id=144854, impetus='metadata').check_kernel_tag())
