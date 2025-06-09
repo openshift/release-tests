7ad5da4c6df6189753db6737ef94367b7c059833
Tomas David <tdavid@redhat.com>
2025-06-09 16:45:37 +0200

    OCPERT-97 Add notify-managers command (#541)
    * OCPERT-97 Add notify-managers command
    
    * Add missing cve text
    
    * Add missing ldap3 dependency
    
    * Fix capitalized list
    
    * Add checks for parameters
    
    * Remove unimplemented command notify-team-leads
    
    * Fix missing space
    
    * Add more tests
    
    * Fix typo
    
    * Add check for user id
    
    * Add raises docs
    
    * Disable sharing notification if list is empty
    
    * Simplify ldap and do not interrupt process
    
    * Return None if manager id not found
    
    * Catch JiraUnauthorizedException

--- oar/cli/cmd_update_bug_list.py
+++ oar/cli/cmd_update_bug_list.py
@@ -16,12 +16,15 @@ logger = logging.getLogger(__name__)
 @click.option("--notify/--no-notify", default=True, help="Send notification to bug owners, default value is true")
 @click.option("--confirm-droppable", is_flag=True, default=False,
               help="Send notification only to bug owners with critical and higher issue severity, default value is false")
-def update_bug_list(ctx, notify, confirm_droppable):
+@click.option("--notify-managers", is_flag=True, default=False, help="Send notification to managers of unverified CVE issues, default value is false")
+def update_bug_list(ctx, notify, confirm_droppable, notify_managers):
     """
     Update bug status listed in report, update existing bug status and append new ON_QA bug
     """
-    if not notify and confirm_droppable:
-        raise click.UsageError("Error: --no-notify and --confirm-droppable cannot be used together")
+    if not notify and (confirm_droppable or notify_managers):
+        raise click.UsageError("Error: --no-notify cannot be used together with --confirm-droppable or --notify-managers")
+    if sum([confirm_droppable, notify_managers]) > 1:
+        raise click.UsageError("Error: only one of parameters --confirm-droppable or --notify-managers can be used simultaneously")
     # get config store from context
     cs = ctx.obj["cs"]
     try:
@@ -36,7 +39,16 @@ def update_bug_list(ctx, notify, confirm_droppable):
         if notify:
             if confirm_droppable:
                 high_severity_issues, _ = JiraManager(cs).get_high_severity_and_can_drop_issues(jira_issues)
-                NotificationManager(cs).share_high_severity_bugs(high_severity_issues)
+                if len(high_severity_issues):
+                    NotificationManager(cs).share_high_severity_bugs(high_severity_issues)
+                else:
+                    logger.info("No high severity issues found.")
+            elif notify_managers:
+                unverified_cve_issues = JiraManager(cs).get_unverified_cve_issues(jira_issues)
+                if len(unverified_cve_issues):
+                    NotificationManager(cs).share_unverified_cve_issues_to_managers(unverified_cve_issues)
+                else:
+                    logger.info("No unverified CVE issues found.")
             else:
                 NotificationManager(cs).share_bugs_to_be_verified(jira_issues)
         # check if all bugs are verified
