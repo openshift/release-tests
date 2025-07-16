79c2f04409213286faeec5266e9c710faa0d3058
Tomas David <tdavid@redhat.com>
2025-07-16 10:56:35 +0200

    Add more tests and improvements

--- tests/test_jira_notificator.py
+++ tests/test_jira_notificator.py
@@ -10,22 +10,51 @@ class TestJiraNotificator(unittest.TestCase):
     def setUp(self):
         jira_token = os.environ.get("JIRA_TOKEN")
         self.jira = JIRA(server="https://issues.redhat.com", token_auth=jira_token)
-    
-    def test_notification_type(self):
+        self.test_issue = self.jira.issue("OCPBUGS-59288", expand="changelog")
+        self.test_issues_without_qa = self.jira.issue("OCPBUGS-8760", expand="changelog")
+
+    def test_create_notification_title(self):
+        self.assertEqual(
+            create_notification_title(NotificationType.QA_CONTACT),
+            (
+                "Errata Reliability Team Notification - QA Contact Action Request\n"
+                "This issue has been in the ON_QA state for over 24 hours."
+            )
+        )
+        self.assertEqual(
+            create_notification_title(NotificationType.TEAM_LEAD),
+            (
+                "Errata Reliability Team Notification - Team Lead Action Request\n"
+                "This issue has been in the ON_QA state for over 48 hours."
+            )
+        )
+        self.assertEqual(
+            create_notification_title(NotificationType.MANAGER),
+            (
+                "Errata Reliability Team Notification - Manager Action Request\n"
+                "This issue has been in the ON_QA state for over 72 hours."
+            )
+            
+        )
+
+    def test_get_notification_type(self):
         self.assertEqual(get_notification_type("QA Contact Action Request"), NotificationType.QA_CONTACT)
         self.assertEqual(get_notification_type("Team Lead Action Request"), NotificationType.TEAM_LEAD)
         self.assertEqual(get_notification_type("Manager Action Request"), NotificationType.MANAGER)
         self.assertEqual(get_notification_type("Please verify the issues as soon as possible."), None)
+        self.assertEqual(get_notification_type("QA Contact Manager Team Lead"), None)
         self.assertEqual(get_notification_type(""), None)
 
+    def test_find_user_by_email(self):
+        self.assertEqual(find_user_by_email(self.jira, "tdavid@redhat.com").displayName, "Tomas David")
+        self.assertEqual(find_user_by_email(self.jira, "dtomas@redhat.com"), None)
+
     def test_get_qa_contact(self):
-        issue = self.jira.issue("OCPBUGS-59288", expand="changelog")
-        qa_contact = get_qa_contact(issue)
+        qa_contact = get_qa_contact(self.test_issue)
         self.assertEqual(qa_contact.displayName, "Tomas David")
         self.assertEqual(qa_contact.name, "tdavid@redhat.com")
 
-        issues_without_qa = self.jira.issue("OCPBUGS-8760", expand="changelog")
-        self.assertEqual(get_qa_contact(issues_without_qa), None)
+        self.assertEqual(get_qa_contact(self.test_issues_without_qa), None)
 
     def test_get_manager(self):
         qa_contact = Mock()
@@ -38,72 +67,68 @@ class TestJiraNotificator(unittest.TestCase):
         invalid_user.emailAddress = "dtomas@redhat.com"
         self.assertEqual(get_manager(self.jira, invalid_user), None)
 
-    def test_find_user_by_email(self):
-        self.assertEqual(find_user_by_email(self.jira, "tdavid@redhat.com").displayName, "Tomas David")
-        self.assertEqual(find_user_by_email(self.jira, "dtomas@redhat.com"), None)
-    
-
-    def test_create_notification_prefix(self):
-        self.assertEqual(
-            create_notification_prefix(NotificationType.QA_CONTACT),
-            "Errata Reliability Team Notification - QA Contact Action Request: This issue has been in the ON_QA state for over 24 hours."
-        )
-        self.assertEqual(
-            create_notification_prefix(NotificationType.TEAM_LEAD),
-            "Errata Reliability Team Notification - Team Lead Action Request: This issue has been in the ON_QA state for over 48 hours."
-        )
-        self.assertEqual(
-            create_notification_prefix(NotificationType.MANAGER),
-            "Errata Reliability Team Notification - Manager Action Request: This issue has been in the ON_QA state for over 72 hours."
-        )
-
     def test_create_qa_notification_text(self):
         self.assertEqual(
-            create_qa_notification_text(self.jira.issue("OCPBUGS-59288", expand="changelog")),
+            create_qa_notification_text(self.test_issue),
             (
-                "Errata Reliability Team Notification - QA Contact Action Request: This issue has been in the ON_QA state for over 24 hours. "
-                "[~tdavid@redhat.com] Please verify the Jira as soon as possible."
+                "Errata Reliability Team Notification - QA Contact Action Request\n"
+                "This issue has been in the ON_QA state for over 24 hours.\n"
+                "[~tdavid@redhat.com] Please verify the Jira ticket as soon as possible."
             )
         )
         self.assertEqual(
-            create_qa_notification_text(self.jira.issue("OCPBUGS-8760", expand="changelog")),
+            create_qa_notification_text(self.test_issues_without_qa),
             (
-                "Errata Reliability Team Notification - QA Contact Action Request: This issue has been in the ON_QA state for over 24 hours. "
-                "Please verify the Jira as soon as possible."
+                "Errata Reliability Team Notification - QA Contact Action Request\n"
+                "This issue has been in the ON_QA state for over 24 hours.\n"
+                "Please verify the Jira ticket as soon as possible."
             )
         )
-    
+
     def test_create_team_lead_notification_text(self):
         self.assertEqual(
             create_team_lead_notification_text(self.jira.issue("OCPBUGS-59288", expand="changelog")),
             (
-                "Errata Reliability Team Notification - Team Lead Action Request: This issue has been in the ON_QA state for over 48 hours. "
-                "[~tdavid@redhat.com] Please verify the Jira as soon as possible or arrange a reassignment with your team lead."
+                "Errata Reliability Team Notification - Team Lead Action Request\n"
+                "This issue has been in the ON_QA state for over 48 hours.\n"
+                "[~tdavid@redhat.com] Please verify the Jira ticket as soon as possible or arrange a reassignment with your team lead."
             )
         )
         self.assertEqual(
             create_team_lead_notification_text(self.jira.issue("OCPBUGS-8760", expand="changelog")),
             (
-                "Errata Reliability Team Notification - Team Lead Action Request: This issue has been in the ON_QA state for over 48 hours. "
-                "Please verify the Jira as soon as possible or arrange a reassignment with your team lead."
+                "Errata Reliability Team Notification - Team Lead Action Request\n"
+                "This issue has been in the ON_QA state for over 48 hours.\n"
+                "Please verify the Jira ticket as soon as possible or arrange a reassignment with your team lead."
             )
         )
-    
+
     def test_create_manager_notification_text(self):
         self.assertEqual(
             create_manager_notification_text(self.jira, self.jira.issue("OCPBUGS-59288", expand="changelog")),
             (
-                "Errata Reliability Team Notification - Manager Action Request: This issue has been in the ON_QA state for over 72 hours. "
-                "[~rhn-support-gjospin] Please prioritize the verification or consider reassigning it to another available QA Contact."
+                "Errata Reliability Team Notification - Manager Action Request\n"
+                "This issue has been in the ON_QA state for over 72 hours.\n"
+                "[~rhn-support-gjospin] Please prioritize the Jira ticket verification or consider reassigning it to another available QA Contact."
             )
         )
         self.assertEqual(
             create_manager_notification_text(self.jira, self.jira.issue("OCPBUGS-8760", expand="changelog")),
             (
-                "Errata Reliability Team Notification - Manager Action Request: This issue has been in the ON_QA state for over 72 hours. "
-                "Please prioritize the verification or consider reassigning it to another available QA Contact."
+                "Errata Reliability Team Notification - Manager Action Request\n"
+                "This issue has been in the ON_QA state for over 72 hours.\n"
+                "Please prioritize the Jira ticket verification or consider reassigning it to another available QA Contact."
             )
         )
+
+    def test_get_latest_on_qa_transition_datetime(self):
+        self.assertEqual(get_latest_on_qa_transition_datetime(self.test_issue), datetime(2025, 7, 15, 14, 10, 20, 862000, timezone.utc))
+        self.assertEqual(get_latest_on_qa_transition_datetime(self.test_issues_without_qa), None)
+
+    def test_get_latest_ert_notification_type_after_on_qa_transition(self):
+        self.assertEqual(get_latest_ert_notification_type_after_on_qa_transition(self.test_issue, datetime(2025, 7, 15, 14, 50, 0, 0, timezone.utc)), NotificationType.TEAM_LEAD)
+        self.assertEqual(get_latest_ert_notification_type_after_on_qa_transition(self.test_issue, datetime(2025, 7, 15, 14, 52, 0, 0, timezone.utc)), None)
+        self.assertEqual(get_latest_ert_notification_type_after_on_qa_transition(self.test_issues_without_qa, datetime(2024, 1, 1, 1, 1, 0, 0, timezone.utc)), None)
 
     def test_get_on_qa_issues(self):
         issues = get_on_qa_issues(self.jira)
--- tools/jira_notificator.py
+++ tools/jira_notificator.py
@@ -21,8 +21,11 @@ class NotificationType(Enum):
         self.hours = hours
         self.label = label
 
-def create_notification_prefix(notification_type: NotificationType) -> str:
-    return f"{ERT_NOTIFICATION_PREFIX} - {notification_type.label}: This issue has been in the ON_QA state for over {notification_type.hours} hours."
+def create_notification_title(notification_type: NotificationType) -> str:
+    return (
+        f"{ERT_NOTIFICATION_PREFIX} - {notification_type.label}\n"
+        f"This issue has been in the ON_QA state for over {notification_type.hours} hours."
+    )
 
 def get_notification_type(notification_message: str) -> Optional[NotificationType]:
     for notification_type in NotificationType:
@@ -30,7 +33,7 @@ def get_notification_type(notification_message: str) -> Optional[NotificationTyp
             return notification_type
     return None
 
-def find_user_by_email(jira: JIRA, email) -> Optional[User]:
+def find_user_by_email(jira: JIRA, email: str) -> Optional[User]:
     for user in jira.search_users(user=email):
         if user.emailAddress == email:
             return user
@@ -42,7 +45,7 @@ def get_qa_contact(issue: Issue) -> Optional[User]:
     if qa_contact:
         return qa_contact
     else:
-        logger.warning(f"Issue {issue.key} does not have QA contact.")
+        logger.warning(f"Issue {issue.key} does not have a QA contact.")
         return None
 
 def get_manager(jira: JIRA, qa_contact: User) -> Optional[User]:
@@ -53,24 +56,31 @@ def get_manager(jira: JIRA, qa_contact: User) -> Optional[User]:
         if manager:
             return manager
         else:
-            logger.warning("Notification to QA Contact Manager could not be sent because manager user was not found in Jira.")
-    
+            logger.warning(f"Manager {manager_email} was not found in Jira.")
     else:
-        logger.warning("Notification to QA Contact Manager could not be sent because QA contact manager was not found.")
+        logger.warning(f"Manager of QA contact {qa_contact.emailAddress} was not found in LDAP.")
     return None
 
 def create_qa_notification_text(issue: Issue)  -> str:
+    qa_contact_link = ""
     qa_contact = get_qa_contact(issue)
-    qa_contact_link = f"[~{qa_contact.name}] " if qa_contact else ""
-    return f"{create_notification_prefix(NotificationType.QA_CONTACT)} {qa_contact_link}Please verify the Jira as soon as possible."
+    if qa_contact:
+        qa_contact_link = f"[~{qa_contact.name}] "
+    else:
+        logger.warning(f"Notification text was generated without a link to the QA Contact for Jira ticket {issue.key}.")
+    return f"{create_notification_title(NotificationType.QA_CONTACT)}\n{qa_contact_link}Please verify the Jira ticket as soon as possible."
 
 def notify_qa_contact(jira: JIRA, issue: Issue) -> None:
     jira.add_comment(issue, create_qa_notification_text(issue))
 
 def create_team_lead_notification_text(issue: Issue) -> str:
+    qa_contact_link = ""
     qa_contact = get_qa_contact(issue)
-    qa_contact_link = f"[~{qa_contact.name}] " if qa_contact else ""
-    return f"{create_notification_prefix(NotificationType.TEAM_LEAD)} {qa_contact_link}Please verify the Jira as soon as possible or arrange a reassignment with your team lead."
+    if qa_contact:
+        qa_contact_link = f"[~{qa_contact.name}] "
+    else:
+        logger.warning(f"Notification text was generated without a link to the QA Contact for Jira ticket {issue.key}.")
+    return f"{create_notification_title(NotificationType.TEAM_LEAD)}\n{qa_contact_link}Please verify the Jira ticket as soon as possible or arrange a reassignment with your team lead."
 
 def notify_team_lead(jira: JIRA, issue: Issue) -> None:
     logger.info("Notification to the team lead is not yet implemented. Notifying the QA contact again.")
@@ -81,10 +91,13 @@ def create_manager_notification_text(jira: JIRA, issue: Issue) -> str:
     manager_link = ""
     if qa_contact:
         manager = get_manager(jira, qa_contact)
-        manager_link = f"[~{manager.name}] " if manager else ""
+        if manager:
+            manager_link = f"[~{manager.name}] "
+        else:
+            logger.warning(f"Notification text was generated without a link to the Manager for Jira ticket {issue.key} (Missing Manager).")
     else:
-        logger.warning("Notification to QA Contact Manager could not be sent because of missing QA contact.")
-    return f"{create_notification_prefix(NotificationType.MANAGER)} {manager_link}Please prioritize the verification or consider reassigning it to another available QA Contact."
+        logger.warning(f"Notification text was generated without a link to the Manager for Jira ticket {issue.key} (Missing QA Contact).")
+    return f"{create_notification_title(NotificationType.MANAGER)}\n{manager_link}Please prioritize the Jira ticket verification or consider reassigning it to another available QA Contact."
 
 def notify_manager(jira: JIRA, issue: Issue) -> None:
     jira.add_comment(issue, create_manager_notification_text(jira, issue))
@@ -105,8 +118,8 @@ def get_latest_on_qa_transition_datetime(issue: Issue) -> Optional[datetime]:
     return latest_on_qa
 
 def get_latest_ert_notification_type_after_on_qa_transition(issue: Issue, on_qa_transition: datetime) -> Optional[NotificationType]:
-    latest_notification_type = None
-    latest_datetime = None
+    latest_notification_type: NotificationType = None
+    latest_datetime: datetime = None
 
     for comment in issue.fields.comment.comments:
         if not comment.body.startswith(ERT_NOTIFICATION_PREFIX):
@@ -142,15 +155,15 @@ def check_issue_and_notify_responsible_people(jira: JIRA, issue: Issue) -> None:
             notify_qa_contact(jira, issue)
 
 def get_on_qa_issues(jira: JIRA) -> list[Issue]:
-    ON_QA_ISSUES_FILTER = "project = OCPBUGS AND issuetype in (Bug, Vulnerability) AND status = ON_QA AND affectedVersion in (4.12.z, 4.13.z, 4.15.z, 4.16.z, 4.17.z, 4.18.z, 4.19.z)"
+    ON_QA_ISSUES_FILTER = "project = OCPBUGS AND issuetype in (Bug, Vulnerability) AND status = ON_QA AND 'Target Version' in (4.12.z, 4.13.z, 4.14.z, 4.15.z, 4.16.z, 4.17.z, 4.18.z, 4.19.z)"
 
     start_at = 0
-    max_results = 100
+    batch_size = 100
     on_qa_issues = []
 
     while True:
         issues = jira.search_issues(
-            ON_QA_ISSUES_FILTER, startAt=start_at, maxResults=max_results, expand="changelog"
+            ON_QA_ISSUES_FILTER, startAt=start_at, maxResults=batch_size, expand="changelog"
         )
         if not issues:
             break
