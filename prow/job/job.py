71ee5ec0fcc8e5b570021829dd67f65d628e3f67
Tomas David <tdavid@redhat.com>
2025-12-09 15:56:08 +0100

    fix exception message

--- prow/job/job.py
+++ prow/job/job.py
@@ -365,7 +365,7 @@ class Jobs:
             else:
                 print(f"Error code: {res.status_code}, reason: {res.reason}")
                 if res.status_code == 403:
-                    raise Exception("Please check the Prow token. Error code: {res.status_code}, reason: {res.reason}")
+                    raise Exception(f"Please check the Prow token. Error code: {res.status_code}, reason: {res.reason}")
         else:
             print(f"Warning! Couldn't find job: {job_name}")
 
